926d0403c24b3edbe6aecf6bfde3f0fa6eb2de39
Pavlo Monastyrskyi <ext-pavlo.monastyrskyi@here.com>
2017-12-12 12:56:09 +0200

    Spelling

--- src/PowderBot/BusinessLogic/UnsubscribeStrategy.cs
+++ src/PowderBot/BusinessLogic/UnsubscribeStrategy.cs
@@ -31,7 +31,7 @@ namespace BusinessLogic
                 {
                     var resorts = (new string[] { "all" })
                         .Concat(subscriptions.Select(s => s.GetResortName()));
-                    return (new ListMessage("From what?", "unsb ", resorts), _user);
+                    return (new ListMessage("What from?", "unsb ", resorts), _user);
                 }
             }
             if (_words.Length != 2)
