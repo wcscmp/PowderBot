bd8c391a8f531e590d3b1996d583ce6892edb16c
vzubenkoidiq <zubenko.vv@gmail.com>
2023-01-28 01:24:00 +0100

    Fix

--- src/PowderBot/BusinessLogic/CommandFactory.cs
+++ src/PowderBot/BusinessLogic/CommandFactory.cs
@@ -21,19 +21,20 @@ namespace BusinessLogic
             var lastCommand = user.LastCommand;
             user.LastCommand = null;
             var words = message
-                .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(s => s.ToLowerInvariant());
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Trim().ToLowerInvariant());
             switch (words.First())
             {
-            case "/check":
-                return new CheckStrategy(user, _subscriptionRepo, _snowForecastClient);
-            case "/subscribe":
-                return new SubscribeStrategy(chatId, user, words.ToArray(), _subscriptionRepo);
-            case "/unsubscribe":
-                return new UnsubscribeStrategy(chatId, user, words.ToArray(), _subscriptionRepo);
-            case "/list":
-                return new ListStrategy(user, _subscriptionRepo);
+                case "/check":
+                    return new CheckStrategy(user, _subscriptionRepo, _snowForecastClient);
+                case "/subscribe":
+                    return new SubscribeStrategy(chatId, user, words.ToArray(), _subscriptionRepo);
+                case "/unsubscribe":
+                    return new UnsubscribeStrategy(chatId, user, words.ToArray(), _subscriptionRepo);
+                case "/list":
+                    return new ListStrategy(user, _subscriptionRepo);
             }
+
             return lastCommand != null
                 ? Create(chatId, user, $"{lastCommand} {message}")
                 : new UsageStrategy(user);
